eb25bb39b0460a0f27ac98d859d54650c90faa2d
Joshua Holtsclaw <jildsmiles@yahoo.com>
2023-03-08 11:03:22 -0500

    attempted smooth scrolling, need to research more. Fixed font size of main title to better fit mobile devices

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Header from '@/components/landingPage/Header';
 import Main from '@/components/landingPage/Main';
 import Footer from '@/components/landingPage/Footer';
+
 export default function Home() {
 	return (
-		<>
+		<div>
 			<Header />
 			<Main />
 			<Footer />
-		</>
+		</div>
 	);
 }
