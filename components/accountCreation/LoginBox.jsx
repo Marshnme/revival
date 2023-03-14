0f1b1c3dddcc473adc32d91e6073c591c021544f
Joshua Holtsclaw <jildsmiles@yahoo.com>
2023-03-14 15:08:07 -0400

    tracking signup and login text inputs

--- components/accountCreation/LoginBox.jsx
+++ components/accountCreation/LoginBox.jsx
@@ -1,17 +1,43 @@
 import styles from '@/components/accountCreation/loginBox.module.css';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 const LoginBox = () => {
 	const router = useRouter();
+
+	const [loginInputs, setLoginInputs] = useState({
+		email: '',
+		password: '',
+	});
+
+	function handleChange(e) {
+		const name = e.target.name;
+
+		// Use this if you need to get value of a checked value
+		// const value =
+		// 	evt.target.type === 'checkbox'
+		// 		? evt.target.checked
+		// 		: evt.target.value;
+
+		const value = e.target.value;
+		setLoginInputs({
+			...loginInputs,
+			[name]: value,
+		});
+	}
+
 	return (
 		<>
 			<section className={styles.loginBox}>
 				<form action="" className={styles.loginForm}>
 					<div>
-						<label htmlFor="">Username*</label>
+						<label htmlFor="">Email*</label>
 						<input
 							required
 							type="text"
-							placeholder="Enter your Username"
+							name="email"
+							placeholder="Enter your Email"
+							value={loginInputs.email}
+							onChange={handleChange}
 						/>
 					</div>
 					<div>
@@ -19,7 +45,10 @@ const LoginBox = () => {
 						<input
 							required
 							type="password"
+							name="password"
 							placeholder="Enter your Password"
+							value={loginInputs.password}
+							onChange={handleChange}
 						/>
 					</div>
 					<div className={styles.loginButtonContainer}>
--- components/accountCreation/SignupBox.jsx
+++ components/accountCreation/SignupBox.jsx
@@ -1,6 +1,29 @@
 import styles from '@/components/accountCreation/signupBox.module.css';
-
+import { useState } from 'react';
 const SignupBox = () => {
+	const [signUpInputs, setSignUpInputs] = useState({
+		email: '',
+		username: '',
+		password: '',
+		confirmPass: '',
+	});
+
+	function handleChange(e) {
+		const name = e.target.name;
+
+		// Use this if you need to get value of a checked value
+		// const value =
+		// 	evt.target.type === 'checkbox'
+		// 		? evt.target.checked
+		// 		: evt.target.value;
+
+		const value = e.target.value;
+		setSignUpInputs({
+			...signUpInputs,
+			[name]: value,
+		});
+	}
+
 	return (
 		<>
 			<section className={styles.signupBox}>
@@ -9,32 +32,44 @@ const SignupBox = () => {
 						<label htmlFor="">Email*</label>
 						<input
 							required
+							name="email"
 							type="email"
 							placeholder="Enter your Email"
+							value={signUpInputs.email}
+							onChange={handleChange}
 						/>
 					</div>
 					<div>
 						<label htmlFor="">Username*</label>
 						<input
 							required
+							name="username"
 							type="text"
 							placeholder="Enter your Username"
+							value={signUpInputs.username}
+							onChange={handleChange}
 						/>
 					</div>
 					<div>
 						<label htmlFor="">Password*</label>
 						<input
 							required
+							name="password"
 							type="password"
 							placeholder="Enter your Password"
+							value={signUpInputs.password}
+							onChange={handleChange}
 						/>
 					</div>
 					<div>
 						<label htmlFor="">Confirm Password*</label>
 						<input
 							required
+							name="confirmPass"
 							type="password"
 							placeholder="Re-enter Password"
+							value={signUpInputs.confirmPass}
+							onChange={handleChange}
 						/>
 					</div>
 					<div className={styles.signupButtonContainer}>
